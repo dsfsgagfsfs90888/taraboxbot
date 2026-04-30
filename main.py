798a3133d6a63a4463d3633c6a8f579509a1dbdc
dsfsgagfsfs90888 <ali732123a@gmail.com>
2026-05-01 00:36:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
-from telegram.ext import ApplicationBuilder, CommandHandler
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-async def start(update, context):
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("👋 Bot is Live!")
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
+
 app.add_handler(CommandHandler("start", start))
 
 print("Bot Running...")
